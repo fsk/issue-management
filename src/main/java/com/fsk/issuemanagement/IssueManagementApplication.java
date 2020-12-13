ac881c170ca526dd2ae903b422e48ca3000d1d8c
frknshnklksz <furkankulaksiz68@gmail.com>
2020-12-13 19:59:13 +0300

    Create Test Data

--- src/main/java/com/fsk/issuemanagement/IssueManagementApplication.java
+++ src/main/java/com/fsk/issuemanagement/IssueManagementApplication.java
@@ -5,6 +5,9 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
 
 @SpringBootApplication
 //@EnableSwagger2WebMvc
@@ -19,5 +22,12 @@ public class IssueManagementApplication {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         return new ModelMapper();
+    }
+
+    @Bean
+    public Jackson2RepositoryPopulatorFactoryBean repositoryPopulator(){
+        Jackson2RepositoryPopulatorFactoryBean factoryBean = new Jackson2RepositoryPopulatorFactoryBean();
+        factoryBean.setResources(new Resource[]{new ClassPathResource("issue-management-test-data.json")});
+        return factoryBean;
     }
 }
